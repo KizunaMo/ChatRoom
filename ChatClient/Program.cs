ab29b38fffb20a406b5680adcae21b5691935aac
AMO <mo791206@gmail.com>
2021-11-26 12:56:50 +0800

    practice

---
+++ ChatClient/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string hostIP = "127.0.0.1";
+            const int port = 4099;
+
+            Console.WriteLine("--------");
+            TcpClient client = new TcpClient();
+
+            try
+            {
+                Console.WriteLine($"連線到聊天伺服器 {hostIP}:{port}");
+                client.Connect(hostIP, port);
+
+                if (!client.Connected)
+                {
+                    Console.WriteLine($"無法連線到聊天伺服器");
+                    return;
+                }
+                Console.WriteLine($"已連線到聊天伺服器");
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"ArgumentException : {e}");
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine($"SocketException : {e}");
+            }
+            finally
+            {
+                client.Close();
+                Console.WriteLine($"連線中斷");
+            }
+        }
+        private static void Send(TcpClient client , string message)
+        {
+            byte[] requestBuffer = Encoding.ASCII.GetBytes(message);//轉換ASCII成byte
+            client.GetStream().Write(requestBuffer, 0, requestBuffer.Length);
+        }
+
+
+    }
+}
---
+++ ChatCore/ChatClient.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatCore
+{
+    public class ChatClient
+    {
+        private TcpClient client;
+
+        public ChatClient()
+        {
+
+        }
+
+        public bool Connect(string address,int port)
+        {
+            client = new TcpClient();
+
+            try
+            {
+                Console.WriteLine($"連線到聊天伺服器 {address}:{port}");
+                client.Connect(address, port);
+
+                Console.WriteLine($"已連線到聊天伺服器");
+                return client.Connected;
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"ArgumentException : {e}");
+                return false;
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine($"SocketException : {e}");
+                return false;
+            }
+        }
+        
+        public void Disconnect()
+        {
+            client.Close();
+            Console.WriteLine($"已中斷連線");
+        }
+
+        public void SendData(string message)
+        {
+            byte[] requestBuffer = Encoding.ASCII.GetBytes(message);
+            client.GetStream().Write(requestBuffer, 0, requestBuffer.Length);
+        }
+
+    }
+}
