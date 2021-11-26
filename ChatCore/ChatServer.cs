0cf456fd199c57488c7a82bcd6aa33df3966ba6e
AMO <mo791206@gmail.com>
2021-11-26 20:03:50 +0800

    整理完ChatServer

--- ChatCore/ChatServer.cs
+++ ChatCore/ChatServer.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace ChatCore
 {
-    class ChatServer
+    public class ChatServer
     {
         private int port;
         private TcpListener listener;
--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -6,82 +6,16 @@ using System.Text;
 
 namespace ChatServer
 {
-    class Program
+    internal class Program
     {
         static HashSet<TcpClient> clients = new HashSet<TcpClient>();
 
         static void Main(string[] args)
         {
-            const int port = 4099;
-
-            Console.WriteLine("-----");
-
-            TcpListener listener = new TcpListener(IPAddress.Any, port);
-
-            try
-            {
-                Console.WriteLine($"伺服器 開始於 port{port}");
-                listener.Start();
-
-                Console.WriteLine($"等待連線......");
-                TcpClient client = listener.AcceptTcpClient();
-
-                string address = client.Client.RemoteEndPoint.ToString();
-                Console.WriteLine($"客戶端 已經連限於{address}端");
-
-                client.Close();
-                Console.WriteLine($"取消連線 客戶端{address}");
-
-            }
-            catch (SocketException e)
-            {
-                Console.WriteLine($" 當機 發生於{e}");
-            }
-            finally
-            {
-                //停止監聽新客戶端連線
-                listener.Stop();
-                Console.WriteLine($"伺服器端關閉");
-            }
-        }
-
-        private static void HandleMessage()
-        {
-            while (true)
-            {
-                lock (clients)
-                {
-                    foreach (var client in clients)
-                    {
-                        try
-                        {
-                            if (client.Available > 0)
-                                Recevie(client);
-                        }
-                        catch (Exception e)
-                        {
-                            Console.WriteLine($"Error : {e}");
-                        }
-                    }
-                }
-            }
-        }
-
-        private static void Recevie(TcpClient client)
-        {
-            NetworkStream stream = client.GetStream();
-            string address = client.Client.RemoteEndPoint.ToString();
-
-            int numBytes = client.Available;
-            if (numBytes == 0)
-                return;
-
-            byte[] buffer = new byte[numBytes];
-            int bytesRead = stream.Read(buffer, 0, numBytes);
-
-            string request = Encoding.ASCII.GetString(buffer).Substring(0, bytesRead);
-            Console.WriteLine($"Text:{request} from {address}");
-
+            Console.WriteLine($"--------");
+            ChatCore.ChatServer server = new ChatCore.ChatServer();
+            server.Bind(4099);
+            server.StartSever();
         }
     }
 }
