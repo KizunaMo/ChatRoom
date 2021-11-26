f1c8f65dbc0f8a6885261720d1dd160659421160
AMO <mo791206@gmail.com>
2021-11-26 18:27:22 +0800

    開始整理ChatServer

--- ChatCore/ChatServer.cs
+++ ChatCore/ChatServer.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ChatCore
 {
     class ChatServer
     {
+        private int port;
+        private TcpListener listener;
+        private Thread handleThread;
+
+        private readonly Dictionary<string, TcpClient> clients = new Dictionary<string, TcpClient>();
+        private readonly Dictionary<string, string> userNames = new Dictionary<string, string>();
+
+        public ChatServer()
+        {
+
+        }
+
+        public void Bind(int port)
+        {
+            this.port = port;
+
+            listener = new TcpListener(IPAddress.Any, port);
+            Console.WriteLine($"伺服器開始於port{port}");
+            listener.Start();
+        }
+
+        public void StartSever()
+        {
+            handleThread = new Thread(ClientsHandler);
+        }
+
+        private void ClientsHandler()
+        {
+
+        }
     }
 }
