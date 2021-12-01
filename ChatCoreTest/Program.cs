9c7a1a9d157ace7fb7bc15278f35ab2a76a23c7b
KizunaMo <mo791206@gmail.com>
2021-12-01 11:27:00 +0800

    列印出錯

--- ChatCoreTest/Program.cs
+++ ChatCoreTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace ChatCoreTest
@@ -8,6 +9,8 @@ namespace ChatCoreTest
         private static byte[] m_PacketData;
         private static uint m_Pos;
         private static int getLengh;
+        private static int index;
+
 
         public static void Main(string[] args)
         {
@@ -33,8 +36,12 @@ namespace ChatCoreTest
         // write an integer into a byte array
         private static bool Write(int i)
         {
+            index = 1;
             // convert int to byte array
             var bytes = BitConverter.GetBytes(i);
+            var bytesIndex = BitConverter.GetBytes(index);
+
+            _Write(bytesIndex);
             _Write(bytes);
             return true;
         }
@@ -42,8 +49,11 @@ namespace ChatCoreTest
         // write a float into a byte array
         private static bool Write(float f)
         {
+            index = 2;
             // convert int to byte array
-            var bytes = BitConverter.GetBytes(f);
+            var bytes = BitConverter.GetBytes(index + f);
+            var bytesIndex = BitConverter.GetBytes(index);
+            _Write(bytesIndex);
             _Write(bytes);
             return true;
         }
@@ -51,8 +61,10 @@ namespace ChatCoreTest
         // write a string into a byte array
         private static bool Write(string s)
         {
+            index = 3;
             // convert string to byte array
-            var bytes = Encoding.Unicode.GetBytes(s);
+            var bytes = Encoding.Unicode.GetBytes(index + s);
+            var bytesIndex = BitConverter.GetBytes(index);
 
             // write byte array length to packet's byte array
             if (Write(bytes.Length) == false)
@@ -60,6 +72,7 @@ namespace ChatCoreTest
                 return false;
             }
 
+            _Write(bytesIndex);
             _Write(bytes);
             return true;
         }
@@ -67,7 +80,7 @@ namespace ChatCoreTest
         // write a byte array into packet's byte array
         private static void _Write(byte[] byteData)
         {
-             GetMessageTotalLengh(byteData);
+            GetMessageTotalLengh(byteData);
             // converter little-endian to network's big-endian
             if (BitConverter.IsLittleEndian)
             {
@@ -77,6 +90,7 @@ namespace ChatCoreTest
             byteData.CopyTo(m_PacketData, m_Pos);
             m_Pos += (uint)byteData.Length;
         }
+
 
         private static void GetMessageTotalLengh(byte[] byteData)
         {
@@ -92,9 +106,12 @@ namespace ChatCoreTest
         private static void Read(byte[] byteData)
         {
             Console.WriteLine($"封包大小為{m_Pos}，封包前四個bytes為紀錄實際數據總長度:{getLengh}");
-            for (int i = 0; i < m_Pos; i++)
+            for (int i = 4; i < m_Pos; i++)
             {
-                Console.Write($"{byteData[i]},");
+                m_Pos = 4;
+                //string message = Encoding.Unicode.GetString(byteData).Substring(0, (int)m_Pos);
+                //Console.Write($"{message},");
+                m_Pos += (uint)byteData.Length;
             }
         }
 
